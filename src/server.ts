7640cbd74016f5bf3ba7d6cd6815491e1de367a8
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-08-04 12:09:10 -0300

    Important Info for starters

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 import express from 'express'
 
 const app = express();
+app.use(express.json())
+
 /*
+||--------------------------------||
 ||         HTTP METHODS           ||
 ||--------------------------------||
 || GET    : Buscar uma informação ||
 || POST   : Cria uma informação   ||
 || PUT    : Edita uma informação  ||
 || DELETE : Delete uma informação ||
+||--------------------------------||
+*/
+
+
+
+/*
+||--------------------------------------------------------------------------||
+|| Request Body => dados para criação ou atualização de registros           ||
+|| Route Params => Identificar qual recurso eu quero atualizar ou deletar   ||
+|| Query Params => Paginação, filtros e ordenação                           ||
+||--------------------------------------------------------------------------||
 */
 app.get('/user', (req, res) =>
 {
